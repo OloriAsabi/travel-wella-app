cd564a21ed0863ee6a3e86fc8fb860f4b6bfb33b
Latifat Oduntan <79084252+latifa-12@users.noreply.github.com>
2022-01-27 20:25:49 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { CssBaseline, Grid } from "@material-ui/core";
+
+import { getPlacesData, getWeatherData } from "./apiCalls";
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
+
 
 function App() {
+  const [places, setPlaces] = useState([])
+  const [weatherData, setWeatherData] = useState([])
+  const [filteredPlaces, setFilteredPlaces] = useState([])
+  const [childClicked, setChildClicked] = useState([])
+
+  const [coords, setCoordinates] = useState({})
+  const [bounds, setBounds] = useState({})
+
+  const [isLoading, setIsLoading] = useState(false)
+  const [type, setType] = useState("restaurants")
+  const [rating, setRating] = useState("")
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
+      setCoordinates({ lat: latitude, lng: longitude })
+      
+    })
+  }, [])
+
+  console.log(coords)
+
+
+  useEffect(() => {
+   const filtered = places?.filter((place) => Number(place.rating) > rating)
+   setFilteredPlaces(filtered);
+  }, [rating, places])
+
+  useEffect(() => {
+    if (bounds) {
+    setIsLoading(true)
+
+    getWeatherData(coords.lat, coords.lng)
+    .then((data) => {
+      setWeatherData(data)
+    })
+
+    getPlacesData(type, bounds.sw, bounds.ne)
+      .then((data) => {
+        console.log({data})
+        setPlaces(data?.filter((place) => place.name && place.num_reviews > 0));
+        setFilteredPlaces([])
+        setRating('');
+        setIsLoading(false)
+      });
+    }
+  },[bounds, type])
+
+  console.log(places)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <CssBaseline/>
+      <Header/>
+      <Grid container spacing={3} style={{ width:"100%" }}>
+        <Grid item xs={12} md={6}>
+          <List 
+          isLoading={isLoading}
+          childClicked={childClicked}
+          places={filteredPlaces?.length ? filteredPlaces : places}
+          type={type}
+          setType={setType}
+          rating={rating}
+          setRating={setRating}
+          />
+        </Grid>
+        <Grid item xs={12} md={6}  style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <Map 
+          setChildClicked={setChildClicked}
+          setCoordinates={setCoordinates}
+          setBounds={setBounds}
+          coords={coords}
+          places={filteredPlaces?.length ? filteredPlaces : places}
+          weatherData={weatherData}
+          />
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
---
+++ src/components/Map/Map.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlined from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab/Rating";
+
+
+import makeStyles from '../../makeStyles';
+import useStyles from "./style";
+
+const Map = ({ setCoordinates, setBounds, coords, places, setChildClicked, weatherData }) => {
+    const classes = useStyles();
+    const isDesktop = useMediaQuery('(min-width:600px)')
+
+
+    return (
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+            bootstrapURLKeys={{ key: "AIzaSyBivzdhGwiCTvYLQ2kXTbd3qFr8nQbJ-Hw" }}
+            center={coords}
+            defaultCenter={coords}
+            defaultZoom={14}
+            margin={[50, 50, 50, 50]}
+            options={{disableDefaultUI: true, zoomControl: true, styles: makeStyles }}
+            onChange={(e) => {
+                setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+                setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+            }}
+            onChildClick={(child) => setChildClicked(child)}
+            >
+                {places?.length && places?.map((place, i) => (
+                    <div className={classes.markerContainer}
+                    lat={Number(place.latitude)}
+                    lng={Number(place.longitude)}
+                    key={i}
+                    >
+                        {!isDesktop ? 
+                                <LocationOnOutlined color="primary" fontSize="large"/>
+                             : (
+                                <Paper elevation={3} className={classes.paper}>
+                                    <Typography className={classes.typography} variant="subtitle2" gutterBottom>
+                                        {place.name}
+                                    </Typography>
+                                    <img 
+                                    className={classes.pointer}
+                                    src={place.photo ? place.photo.images.small.url : 'https://raw.githubusercontent.com/OloriAsabi/book-images/main/src/PROGRAM/download-2.jpg'}
+                                    alt={place.name}  
+                                    />
+                                     <Rating name="read-only" size="small" value={Number(place.rating)} readOnly />
+                                </Paper>
+                            )
+                        }
+                    </div>
+                ))}
+                {weatherData?.list?.length && weatherData?.list?.map((data, i) => (
+                <div key={i} lat={data.coord.lat} lng={data.coord.lon}>
+                    <img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} height="70px" />
+                </div>
+                ))}
+            </GoogleMapReact>
+        </div>
+    )
+}
+
+export default Map
